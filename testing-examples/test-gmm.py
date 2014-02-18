6817db7bb8d7d0390eb4b9f6c9ce7357502d5ecc
BhuvanAnand <bhuvanlive@gmail.com>
2014-02-18 06:55:29 -0800

    creation of gmm object with different initializations tested.

--- testing-examples/test-gmm.py
+++ testing-examples/test-gmm.py
@@ -1,19 +1,39 @@
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '../'))
+
+import sonus.feature.mfcc as mfcc
+import sonus.utils.sonusreader as sonusreader
+import sonus.utils.gmm.gmm as gmm
+
 def main():
     audio = sonusreader.SonusReader.from_file('/home/bhuvan/Desktop/.all-folders/sci-python/smashingbaby.wav')
 
     mfccs = mfcc.mfcc(audio.data, samplerate=audio.samplerate)
+
+    print mfccs[0]
+
+    # crete a GMM object
+    # replace option -> method with 'uniform', 'random', 'kmeans'
+    GMM = gmm.GaussianMixtureModel(
+        data=mfccs, 
+        nClusters=4,
+        options={
+            'method':'kmeans'
+            })
     
-    print mfccs
-    
+    print GMM.models[0].mean
 
 if __name__ == '__main__':
-    import os
-    import sys
-
-    sys.path.append(os.path.join(os.path.dirname(__file__), '../'))
-
-    import sonus.feature.mfcc as mfcc
-    import sonus.utils.sonusreader as sonusreader
-    import sonus.utils.gmm.gmm as gmm
-
     main()
+
+
+
+
+
+
+
+
+
+
