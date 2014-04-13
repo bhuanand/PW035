5c4fcf5bd269e5340b60ae82a1ce40cbd90b731a
BhuvanAnand <bhuvanlive@gmail.com>
2014-04-13 01:30:27 -0700

    added setup.py for installation

---
+++ setup.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='Sonus',
+    version='1.0',
+    description='Language Detection System in python',
+    author='Bhuvan Anand, Krishna Ramesh',
+    packages=['sonus'],
+    classifiers=[
+        'Development Status :: 1.0',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Operating System :: Linux',
+        'Operating System :: Microsoft :: Microsoft Windows',
+        'Operating System :: MacOS :: MacOS X',
+        'Programming Language :: Python 2.7'
+        ]
+    )
